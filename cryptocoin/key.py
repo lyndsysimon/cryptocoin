494bffc73bae6883c3468263bc2ed8c8b2d3d522
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-03-06 21:14:44 -0500

    refactor and document key.py

--- cryptocoin/key.py
+++ cryptocoin/key.py
@@ -7,20 +7,49 @@ class BitcoinKey(object):
 
     _b58_private_key = None
     _passphrase = None
+    _application_byte = b'\x80'
 
     @property
     def passphrase(self):
-        if not self._passphrase:
-            raise ValueError('Passphrase cannot be calculated.')
-        return self._passphrase
+        """ The plaintext passphrase.
+
+        Note that this is available only if a passphrase was passed upon
+        instantiation.
+
+        :raises: ``AttributeError`` if not instantiated with a passphrase.
+        :rtype: str
+        """
+        if self._passphrase:
+            return self._passphrase
+
+        #passphrase must be provided; it can't be calculated.
+        raise AttributeError()
 
     @property
     def private_key(self):
-        if not self._b58_private_key:
+        """ The private key (WIF/wallet import format)
+
+        see: https://en.bitcoin.it/wiki/Wallet_import_format
+
+        :raises: ``AttributeError`` if secret exponent is not known
+        :rtype: str
+        """
+
+        # If the private key is available directly, return it
+        if self._b58_private_key:
+            return self._b58_private_key
+
+        # if the secret exponent exists, generate and cache the private key
+        if self._secret_exponent:
             self._b58_private_key = base58.check_encode(
-                b'\x80' + self._secret_exponent.digest()
+                self._application_byte + self._secret_exponent.digest()
             )
-        return self._b58_private_key
+            return self._b58_private_key
+
+        # can't find or calculate the private key
+        raise AttributeError()
+
+
 
     @property
     def _secret_exponent(self):
