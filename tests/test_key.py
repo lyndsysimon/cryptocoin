30067da0ce4af8f15f4332dc39802fa83d7da5fd
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-03-06 21:14:58 -0500

    refactor and expand tests

--- tests/test_key.py
+++ tests/test_key.py
@@ -3,31 +3,52 @@ import unittest
 
 from cryptocoin.key import BitcoinKey
 
+PASSPHRASE = 'correct horse battery staple'
+SECRET_EXPONENT = (
+    'c4bbcb1fbec99d65bf59d85c8cb62ee2db963f0fe106f483d9afa73bd4e39a8a'
+)
+PRIVATE_KEY = '5KJvsngHeMpm884wtkJNzQGaCErckhHJBGFsvd3VyK5qMZXj3hS'
 
-class PassphraseTestCase(unittest.TestCase):
+
+class KeyTestCase(unittest.TestCase):
+
+    __test__ = False
+
+    def test_secret_exponent(self):
+        with assert_raises(AttributeError):
+            _ = self.key.secret_exponent
+
+    def test_private_key(self):
+        with assert_raises(AttributeError):
+            _ = self.key.private_key
+
+
+@istest
+class PassphraseTestCase(KeyTestCase):
     def setUp(self):
-        self.key = BitcoinKey(passphrase='correct horse battery staple')
+        self.key = BitcoinKey(passphrase=PASSPHRASE)
 
     def test_secret_exponent(self):
         assert_equal(
             self.key.secret_exponent,
-            'c4bbcb1fbec99d65bf59d85c8cb62ee2db963f0fe106f483d9afa73bd4e39a8a',
+            SECRET_EXPONENT,
         )
 
     def test_private_key(self):
         assert_equal(
             self.key.private_key,
-            '5KJvsngHeMpm884wtkJNzQGaCErckhHJBGFsvd3VyK5qMZXj3hS',
+            PRIVATE_KEY,
         )
 
 
-class PrivateKeyTestCase(unittest.TestCase):
+@istest
+class PrivateKeyTestCase(KeyTestCase):
     def setUp(self):
         self.key = BitcoinKey(
-            private_key='5KJvsngHeMpm884wtkJNzQGaCErckhHJBGFsvd3VyK5qMZXj3hS')
+            private_key=PRIVATE_KEY)
 
     def test_private_key(self):
         assert_equal(
             self.key.private_key,
-            '5KJvsngHeMpm884wtkJNzQGaCErckhHJBGFsvd3VyK5qMZXj3hS',
+            PRIVATE_KEY,
         )
