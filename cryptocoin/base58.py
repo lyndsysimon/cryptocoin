4eab25f29a19efb427f37084a88bc718fc1e9872
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-03-10 17:09:08 -0400

    Remove debugging constants

--- cryptocoin/base58.py
+++ cryptocoin/base58.py
@@ -1,13 +1,6 @@
 from binascii import unhexlify, hexlify
 from hashlib import sha256
 
-
-SECRET_EXPONENT = (
-    '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
-)
-PRIVATE_KEY = '5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ'
-
-
 _alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 _radix = len(_alphabet)
 
