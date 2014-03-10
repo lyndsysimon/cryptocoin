16a11a3967cab67a300a4a287fed1da487fc89f9
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-03-10 17:08:15 -0400

    Add tox; proper package setup

--- cryptocoin/__init__.py
+++ cryptocoin/__init__.py
@@ -1,4 +1,5 @@
 from .key import key_factory
 
-
 BitcoinKey = key_factory('Bitcoin', application_byte='80')
+
+__version__ = '0.0.0.dev0'
---
+++ setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+# Get the long description from the relevant file
+# with codecs.open('DESCRIPTION.rst', encoding='utf-8') as f:
+#     long_description = f.read()
+long_description = 'placeholder'
+
+setup(
+    name='cryptocoin',
+    version=find_version('cryptocoin', '__init__.py'),
+    description="Cryptocurrencies for Humans",
+    long_description=long_description,
+
+    url='http://github.com/lyndsysimon/cryptocoin',
+
+    license='Apache',
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+
+    keywords='bitcoin cryptocurrency',
+
+    packages=['cryptocoin'],
+
+    install_requires=['ecdsa', ],
+)
