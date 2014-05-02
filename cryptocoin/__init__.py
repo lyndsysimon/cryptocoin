03589b7ce548d1d881aece614391c2e10e41529c
Lyndsy Simon <lyndsy@lyndsysimon.com>
2014-05-02 18:39:34 -0400

    bump version

--- cryptocoin/__init__.py
+++ cryptocoin/__init__.py
@@ -3,4 +3,4 @@ from .key import key_factory
 BitcoinKey = key_factory('Bitcoin', pubkey_hash='00', privkey_hash='80')
 LitecoinKey = key_factory('Litecoin', pubkey_hash='30', privkey_hash='B0')
 
-__version__ = '0.0.0.dev3'
+__version__ = '0.0.1'
